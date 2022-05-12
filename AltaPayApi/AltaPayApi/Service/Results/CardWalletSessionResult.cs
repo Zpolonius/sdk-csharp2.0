36b298b3ac2a468c388269ff12645c75b09c92fc
Mahad Ashraf <maf@technologies.dk>
2022-05-12 04:47:32 +0000

    update CardWalletSession response class

--- AltaPayApi/AltaPayApi/Service/Results/CardWalletSessionResult.cs
+++ AltaPayApi/AltaPayApi/Service/Results/CardWalletSessionResult.cs
@@ -3,10 +3,28 @@ using AltaPay.Service.Dto;
 
 namespace AltaPay.Service
 {
-	public class CardWalletSessionResult : PaymentResult
+	public class CardWalletSessionResult : ApiResult
 	{
-		public CardWalletSessionResult(APIResponse apiResponse) : base(apiResponse)
+		public string ApplePaySession { get; set; }
+
+		public CardWalletSessionResult(APIResponse apiResponse)
 		{
+			if (apiResponse.Header.ErrorCode == 0)
+			{
+				ResultMessage = apiResponse.Body.CardHolderErrorMessage;
+				ResultMerchantMessage = apiResponse.Body.MerchantErrorMessage;
+
+				if (!String.IsNullOrEmpty(apiResponse.Body.Result))
+					Result = (Result)Enum.Parse(typeof(Result), apiResponse.Body.Result);
+
+				ApplePaySession = apiResponse.Body.ApplePaySession;
+			}
+			else
+			{
+				Result = Result.SystemError;
+				ResultMerchantMessage = apiResponse.Header.ErrorMessage;
+				ResultMessage = "An error occurred";
+			}
 		}
 	}
 }
