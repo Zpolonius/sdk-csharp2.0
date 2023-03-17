c61f266942966976b944def14703766f34617a80
Mahad Ashraf <maf@technologies.dk>
2023-03-17 10:58:23 +0000

    add redirect response params for reservation, chargeSubscription, reserveSubscriptionCharge

--- AltaPayApi/AltaPayApi/Service/Results/ReservationResult.cs
+++ AltaPayApi/AltaPayApi/Service/Results/ReservationResult.cs
@@ -5,11 +5,30 @@ namespace AltaPay.Service
 {
 	public class ReserveResult : PaymentResult
 	{
+        public string RedirectUrl { get; set; }
 
-		public ReserveResult(APIResponse response) : base(response) 
+        public ReserveResult(APIResponse response) : base(response) 
 		{
+            if (response.Header.ErrorCode == 0)
+            {
+                ResultMessage = response.Body.CardHolderErrorMessage;
+                ResultMerchantMessage = response.Body.MerchantErrorMessage;
 
-		}
+                if (!String.IsNullOrEmpty(response.Body.Result))
+                    Result = (Result)Enum.Parse(typeof(Result), response.Body.Result);
+
+                if (response.Body.Result == Result.Redirect.ToString())
+                {
+                    RedirectUrl = response.Body.RedirectUrl;
+                }
+            }
+            else
+            {
+                Result = Result.SystemError;
+                ResultMerchantMessage = response.Header.ErrorMessage;
+                ResultMessage = "An error occurred";
+            }
+        }
 	}
 }
 
